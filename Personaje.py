34a2b6ed60c29ca05bdce72f5d2d08d0626c4699
DiegoMP-235 <121039161@upq.edu.mx>
2023-02-20 17:00:21 -0600

    Se crearon los metodos del personaje

--- Personaje.py
+++ Personaje.py
@@ -3,3 +3,18 @@ class Personaje:
     nombre = "Roberto"
     altura = "1.83"
     
+#Metodos del personaje
+
+def correr(self,status):
+    if(status):
+        print("El personaje "+self.nombre+" esta corriendo")
+    else:
+        print("El personaje "+self.nombre+" esta detenido")    
+        
+def lanzarGranada(self):
+    print("El personaje "+self.nombre+" lanzo una granda...")        
+    
+def rcargarArma(self,municiones):
+    cargador=10
+    cargador+=municiones
+    print("El arma tiene "+cargador+" balas")    
