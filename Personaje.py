932cd92089e8b5e5934c7f743442394dc2dff34e
DiegoMP-235 <121039161@upq.edu.mx>
2023-02-22 17:06:50 -0600

    Se agrego constructor y crearon objetos en el archivo "objtos.py"
    Se agrego el constructor en la clase Personaje y en el archivo "objtos.py" se modifico para pedir datos al usuario e inicializar 2 objetos, uno con los datos del héroe y el otro con los datos del villano.

--- Personaje.py
+++ Personaje.py
@@ -1,10 +1,12 @@
 class Personaje:
-    especie="Humnano"
-    nombre = "Roberto"
-    altura = "1.83"
-    
-#Metodos del personaje
-
+  
+    #Constructor de la clase Personaje
+    def __init__(self,especie,nombre,altura):
+        self.especie = especie
+        self.nombre = nombre
+        self.altura = altura
+        
+     #Metodos del personaje   
     def correr(self,status):
         if(status):
             print("El personaje "+self.nombre+" esta corriendo")
@@ -12,7 +14,7 @@ class Personaje:
             print("El personaje "+self.nombre+" esta detenido")    
         
     def lanzarGranada(self):
-        print("El personaje "+self.nombre+" lanzo una granda...")        
+        print("El personaje "+self.nombre+" lanzo una granda... ")        
     
     def recargarArma(self,municiones):
         cargador=10
--- objtos.py
+++ objtos.py
@@ -1,15 +1,38 @@
 from Personaje import *
+#1.-Solicitamos los datos
+print("######## Crear heroe #########")
+NameH = input("Ingresa el nombre de tu heroe\n->")
+EspecieH = input("Ingresa la especie de tu heroe\n->")
+AlturaH = float(input("Ingresa la altura de tu heroe\n->"))
+RecargaH = int(input("Ingresa las balas que quieres recargar\n->"))
 
-#1.-Creamos obejto de la clase Personaje
+print("######## Crear villano #########")
+NameV = input("Ingresa el nombre de tu villano\n->")
+EspecieV = input("Ingresa la especie de tu villano\n->")
+AlturaV = float(input("Ingresa la altura de tu villano\n->"))
+RecargaV = int(input("Ingresa las balas que quieres recargar al villano\n->"))
 
-myPersonaje = Personaje()
+#2.-Creamos obejto de la clase Personaje
 
-#2.-Usar atributos
-print("El personaje se llama "+myPersonaje.nombre)
-print("El personaje tiene la especie "+myPersonaje.especie)
-print("El personaje tiene altura de "+myPersonaje.altura)
+myHeroe = Personaje(EspecieH,NameH,AlturaH)
+myVillano = Personaje(EspecieV,NameV,AlturaV)
 
-#3.-Usamos los metodos
-myPersonaje.correr(True)
-myPersonaje.lanzarGranada()
-myPersonaje.recargarArma(15)
+#3.-Usar atributos
+print("######## Stats heroe #########")
+print("El personaje se llama "+myHeroe.nombre)
+print("El personaje tiene la especie "+myHeroe.especie)
+print("El personaje tiene altura de "+str(myHeroe.altura))
+
+myHeroe.correr(True)
+myHeroe.lanzarGranada()
+myHeroe.recargarArma(RecargaH)
+
+print("######## Stats villano #########")
+print("El personaje se llama "+myVillano.nombre)
+print("El personaje tiene la especie "+myVillano.especie)
+print("El personaje tiene altura de "+str(myVillano.altura))
+
+myVillano.correr(False)
+myVillano.lanzarGranada()
+myVillano.recargarArma(RecargaV)
+
