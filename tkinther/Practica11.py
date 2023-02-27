0c6e5adacdc59ac3e4977ad970308ca7d7675d1c
DiegoMP-235 <121039161@upq.edu.mx>
2023-02-27 17:14:05 -0600

    Se crearon botones y agregaron a las secciones para probar métodos como grid y pack

--- tkinther/Practica11.py
+++ tkinther/Practica11.py
@@ -14,5 +14,19 @@ seccion2.pack(expand=True,fill='both')
 seccion3 = Frame(ventana,bg="#0000FF")
 seccion3.pack(expand=True,fill='both')
 
+#3.Agregamos botones
+boton1 = Button(seccion1,text="Soy un boton azul",fg="blue",font="bold")
+boton1.place(x = 60,y = 60)
+
+boton2 = Button(seccion2,text="Soy un boton amarillo",bg="#f6ff08",fg="#000000")
+boton2.grid(row = 1,column = 1)
+
+boton3 = Button(seccion2,text="Soy un boton negro",bg="#000000",fg="#FFFFFF")
+boton3.grid(row = 0,column = 0)
+
+botonVerde = Button(seccion3,text="Soy boton verde",bg="#008800",font="Sans-serif",fg="#FFFFFF")
+botonVerde.configure(height=2,width=10)
+botonVerde.pack()
+
 #Llamamos al metodo para mostrar la ventana
 ventana.mainloop()
