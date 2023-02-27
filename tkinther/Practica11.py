632b9cab374960b3466176c88f33de72cd2dbaae
DiegoMP-235 <121039161@upq.edu.mx>
2023-02-27 16:40:51 -0600

    Creamos una carpeta para empezar a trabajar con Tkinter
    Importamos la libreria Tkinter, instanciamos una ventana, establecimos el titulo y las dimensiones de esta.

---
+++ tkinther/Practica11.py
@@ -0,0 +1,9 @@
+from tkinter import Tk,Button,Frame
+
+#1.Instanciamos un objeto de tipo "ventana"
+ventana = Tk()
+ventana.title("Mi primera interfaz grafica (con 3 frames)")
+ventana.geometry("600x400")
+
+#2.Llamamos al metodo para mostrar la ventana
+ventana.mainloop()
