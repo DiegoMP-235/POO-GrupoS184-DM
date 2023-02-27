8a4292d42002ffe62dc7a443abc47a81180c3ff4
DiegoMP-235 <121039161@upq.edu.mx>
2023-02-27 16:47:15 -0600

    Creamos 3 frames para seccionar y probar que las configuraciones funcionen correctamente

--- tkinther/Practica11.py
+++ tkinther/Practica11.py
@@ -4,6 +4,15 @@ from tkinter import Tk,Button,Frame
 ventana = Tk()
 ventana.title("Mi primera interfaz grafica (con 3 frames)")
 ventana.geometry("600x400")
+#2.Agregamos los frames
+seccion1 = Frame(ventana,bg="#FF0000")
+seccion1.pack(expand=True,fill='both')
 
-#2.Llamamos al metodo para mostrar la ventana
+seccion2 = Frame(ventana,bg="#00FF00")
+seccion2.pack(expand=True,fill='both')
+
+seccion3 = Frame(ventana,bg="#0000FF")
+seccion3.pack(expand=True,fill='both')
+
+#Llamamos al metodo para mostrar la ventana
 ventana.mainloop()
