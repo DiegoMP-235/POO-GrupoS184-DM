ed8b2593ef997399cefe6429318cea00212be986
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-01 16:18:43 -0600

    Se creo una funcion para cambiar las propiedades del boton verde y que ademas agrega un boton al frame del mismo boton verde

--- tkinther/Practica11.py
+++ tkinther/Practica11.py
@@ -2,7 +2,17 @@ from tkinter import Tk,Button,Frame,messagebox
 #creamos funciones para probar el command
 def muestraMensaje():
     messagebox.showinfo("Aviso!","Haz presionado el boton")
+    messagebox.showerror("Error","La tarea fallo con exito")
+    messagebox.askquestion("Pregunta","Te gusta la pizza con pinia")
 
+    answer = messagebox.askyesnocancel("Prueba","Si?\nNo?\nCancelar?")
+    print(answer)
+#6.Funcion para agregar botones
+def agregarBoton():
+    botonVerde.config(text="+",bg="#00CC00",fg="#FFFFFF")
+    botonNew = Button(seccion3,text="Nuevo BTN")
+    botonNew.pack()
+    
 #1.Instanciamos un objeto de tipo "ventana"
 ventana = Tk()
 ventana.title("Mi primera interfaz grafica (con 3 frames)")
@@ -28,7 +38,7 @@ boton2.grid(row = 1,column = 1)
 boton3 = Button(seccion2,text="Soy un boton negro",bg="#000000",fg="#FFFFFF")
 boton3.grid(row = 0,column = 0)
 
-botonVerde = Button(seccion3,text="Soy boton verde",bg="#008800",font="Sans-serif",fg="#FFFFFF")
+botonVerde = Button(seccion3,text="Soy boton verde",bg="#008800",font="Sans-serif",fg="#FFFFFF",command=agregarBoton)
 botonVerde.configure(height=2,width=10)
 botonVerde.pack()
 
